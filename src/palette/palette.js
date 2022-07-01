3b9ab9a94d7219334b6b92429523165a04441d9f
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-07-01 21:29:39 +0200

    Update palette.js

--- src/palette/palette.js
+++ src/palette/palette.js
@@ -2,18 +2,49 @@ import lightWindTheme from 'https://lightwindcss.github.io/Palette/src/palette/p
 
 window.addEventListener('load', () => {
     getAllAttributes();
+    lightObserve();
 })
 
 // classes observation functions
-function lightObserve(classes) {
+function lightObserve() {
     new MutationObserver((mutations) => {
         for (let i in mutations) {
-            console.log(mutations[i])
+            if (mutations[i].type == 'childList') {
+                try {
+                    // adding the theme
+                    let theme = mutations[i].target.attributes.theme.nodeValue, element = mutations[i].target
+    
+                    element.style.setProperty('--color1', lightWindTheme[theme].color1)
+                    element.style.setProperty('--color2', lightWindTheme[theme].color2)
+                    element.style.setProperty('--color3', lightWindTheme[theme].color3)
+                    element.style.setProperty('--color4', lightWindTheme[theme].color4)
+                } catch {}
+            }
+            else {
+                if (mutations[i].attributeName == 'theme') {
+                    try {
+                        // adding the theme
+                        let theme = mutations[i].target.attributes.theme.nodeValue, element = mutations[i].target
+        
+                        element.style.setProperty('--color1', lightWindTheme[theme].color1)
+                        element.style.setProperty('--color2', lightWindTheme[theme].color2)
+                        element.style.setProperty('--color3', lightWindTheme[theme].color3)
+                        element.style.setProperty('--color4', lightWindTheme[theme].color4)
+                    } catch {
+                        // deleting the theme
+                        let element = mutations[i].target
+
+                        element.style.removeProperty('--color1')
+                        element.style.removeProperty('--color2')
+                        element.style.removeProperty('--color3')
+                        element.style.removeProperty('--color4')
+                    }
+                }
+            }
         }
     }).observe(document.querySelector('body'), { attributes: true, subtree: true, childList: true });
 }
 function getAllAttributes() {
-    let allAttributes = [];
     let allElements = document.querySelectorAll('*');
     for (let i in allElements) {
         try {
