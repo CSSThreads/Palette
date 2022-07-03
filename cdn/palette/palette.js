6097eba41b12b6d7cc1f45d66989dc1f9180ec3a
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-07-03 13:20:58 +0200

    Update palette.js

--- cdn/palette/palette.js
+++ cdn/palette/palette.js
@@ -2,15 +2,17 @@ import globalJson from 'https://lightwindcss.github.io/Palette/cdn/palette/palet
 
 let lightWindTheme = null;
 
-if (document.querySelector('script[src="https://lightwindcss.github.io/Palette/cdn/palette/palette.js"]').getAttribute('themesrc') == null) {
-    lightWindTheme = globalJson
-}
-else {
-    lightWindTheme = await fetch(document.querySelector('script[src="https://lightwindcss.github.io/Palette/cdn/palette/palette.js"]').getAttribute('themesrc'))
-    .then(response => {
-        return response.json();
-    })
-}
+try {
+    if (document.querySelector('script[src="https://lightwindcss.github.io/Palette/cdn/palette/palette.js"]').getAttribute('themesrc') == null) {
+        lightWindTheme = globalJson
+    }
+    else {
+        lightWindTheme = await fetch(document.querySelector('script[src="https://lightwindcss.github.io/Palette/cdn/palette/palette.js"]').getAttribute('themesrc'))
+        .then(response => {
+            return response.json();
+        })
+    }
+} catch {}
 
 window.addEventListener('load', () => {
     // run the Palette observation on window load
