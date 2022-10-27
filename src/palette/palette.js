0be6eaf968e4013bd5ba7a2f31a9ff140ea1c96f
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-10-27 14:55:39 +0200

    ✨ - Fix attributes standard

--- src/palette/palette.js
+++ src/palette/palette.js
@@ -2,7 +2,7 @@
     // mutations
     (async () => {
         var styleHttpReq = new XMLHttpRequest();
-        styleHttpReq.open("GET", document.querySelector('[themesrc]').getAttribute('themesrc'), false);
+        styleHttpReq.open("GET", document.querySelector('[data-themesrc]').getAttribute('data-themesrc'), false);
         styleHttpReq.send(null);
         let res = JSON.parse(styleHttpReq.responseText)
 
@@ -10,7 +10,7 @@
             mutations.forEach(mutation => {
                 try {
                     mutation.addedNodes.forEach(node => {
-                        let theme = node.attributes.theme.nodeValue
+                        let theme = node.attributes["data-theme"].nodeValue
                         for (var i = 0; i < res[theme].length; i++) {
                             node.style.setProperty(`--color${1+i}`, res[theme][i])
                         }
@@ -33,7 +33,7 @@
 
                     try {
                         // adding the theme colors
-                        let theme = mutation.target.attributes.theme.nodeValue
+                        let theme = mutation.target.attributes["data-theme"].nodeValue
 
                         for (var i = 0; i < res[theme].length; i++) {
                             element.style.setProperty(`--color${1+i}`, res[theme][i])
@@ -41,6 +41,6 @@
                     } catch {}
                 })
             } catch {}
-        }).observe(document, { attributes: true, attributeFilter: ["theme"], subtree: true });
+        }).observe(document, { attributes: true, attributeFilter: ["data-theme"], subtree: true });
     })()
 }
